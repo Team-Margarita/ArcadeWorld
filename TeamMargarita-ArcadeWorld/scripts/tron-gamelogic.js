0b0c8472539497dd3c5189dab16bd68bbd9004c3
Tsvetoslav Lazarov <nearfs@abv.bg>
2016-08-12 00:37:56 +0300

    reset

--- TeamMargarita-ArcadeWorld/scripts/tron-gamelogic.js
+++ TeamMargarita-ArcadeWorld/scripts/tron-gamelogic.js
@@ -2,11 +2,8 @@
 function tron(){
 
   const cellDimension = 10;
-  const pointPerWin = 20;
-  const speedIndex = 100;
-  var isInGame = false;
-  var canvas = document.getElementById("game-canvas");
-  var ctx = canvas.getContext("2d");
+  const pointsPerWin = 20;
+  const speedIndex = 300;
 
   function createPoint (x, y){
     var point = {
@@ -14,6 +11,29 @@ function tron(){
       y : y
     };
     return point;
+  }
+
+  function waitForKey(){
+    var key=0;
+
+    document.addEventListener('keydown', keyDownAny);
+    function keyPressed(e){
+      key = e.keyCode;
+      console.log(key);
+    }
+    while(!key)
+      {}
+    document.removeEventListener('keypress', keyPressed);
+  }
+
+  function printOnCanvas(textToPrint) {
+    let canvas = document.getElementById("game-canvas");
+    let ctx = canvas.getContext("2d");
+
+    ctx.font = "24px ArcadeFont";
+    ctx.fillStyle = '#888888';
+    ctx.textAlign = 'center';
+    ctx.fillText(textToPrint, canvas.width/2, canvas.height/2);
   }
 
   var keysToIndex =  {
@@ -46,6 +66,8 @@ function tron(){
       directionPair : playerDirection,
       wins : initialWins,
       paint : function() {
+        let canvas = document.getElementById("game-canvas");
+        let ctx = canvas.getContext("2d");
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.fillRect(this.position.x * cellDimension, this.position.y * cellDimension, cellDimension, cellDimension);
@@ -56,8 +78,8 @@ function tron(){
         let canvas = document.getElementById("game-canvas");
         let ctx = canvas.getContext("2d");
         var playerHitHimself = positionIsInHistory(this.position, this.history);
-        var playerHitTheWalls = this.position.x<=0||this.position.x>=canvas.width
-                              || this.position.y<=0||this.position.y>=canvas.height;
+        var playerHitTheWalls = this.position.x<=0||this.position.x>=canvas.width/cellDimension
+                              || this.position.y<=0||this.position.y>=canvas.height/cellDimension;
         var playerHitTheOtherPlayer = positionIsInHistory(this.position, otherPlayer.history)
                 ||((this.position.x===otherPlayer.position.x)&&(this.position.y===otherPlayer.position.y));
         return playerHitHimself || playerHitTheWalls || playerHitTheOtherPlayer;
@@ -72,51 +94,46 @@ function tron(){
     return player;
   }
 
-  function drawMsg(){
-      ctx.font = "66px ArcadeFont";
-      ctx.fillStyle = '#FFFFFF';
-      ctx.textAlign = 'center';
-      ctx.fillText("Prepare", canvas.width/2, canvas.height/2);
-    }
-
-    function drawInstruction() {
-        ctx.font = "24px ArcadeFont";
-        ctx.fillStyle = '#888888';
-        ctx.textAlign = 'center';
-        ctx.fillText("Press  ' ENTER '  to  start", canvas.width/2, canvas.height/2 + 30);
-    }
-
-  function resetGame(point){
-    isInGame = false;
-    //point === 1 -> player1 points ++
-    //point === 2 -> player2 points ++
-    //трябва да се reset-ват нещата за играчите (без точките)
-    //if(достигнат брой точки) - извикване на end screen
-    //else reset-ване на играта
+  function paintBoom(point){
+    const boomW = 50;
+    const boomH = 50;
+    let canvas = document.getElementById("game-canvas");
+    let ctx = canvas.getContext("2d");
+    //let boom = new Image();
+    //boom.src = './images/boom.png';
+    //ctx.drawImage(boom, point.x*cellDimension, point.y*cellDimension, boomW, boomH);
+    ctx.beginPath();
+    ctx.fillStyle = '#FF0000';
+    ctx.arc(point.x*cellDimension, point.y*cellDimension, cellDimension*2,0,2*Math.PI);
+    ctx.fill();
+    ctx.closePath();
   }
 
   function tronGameLoop (player1, player2) {
-    if(isInGame){
         player1.paint();
         player2.paint();
-        if (player1.hasCollided(player2)) {
-          resetGame(1)
+        player1.move();
+        player2.move();
+
+        if (player1.hasCollided(player2)&&player2.hasCollided(player1)) {
+          paintBoom(player1.position);
+        }
+        else if (player1.hasCollided(player2)) {
+          player2.wins += 1;
+          paintBoom(player1.position);
           return;
         }
         if (player2.hasCollided(player1)) {
-          resetGame(2);
+          paintBoom(player2.position);
           return;
         }
 
-        player1.move();
-        player2.move();
-    }else{
-      drawMsg();
-      drawInstruction();
-    }
+        //setTimeout(tronGameLoop (player1, player2), 500);
+
+        //requestAnimationFrame(tronGameLoop(player1, player2));
   }
 
-  function playTronGame() {
+  function playTronGame(wins1, wins2) {
 
     var direction = {
       "up" : {
@@ -146,7 +163,7 @@ function tron(){
                               "#FEFF49",
                               direction.right,
     //                          player1Keys,
-                              0);
+                              wins1);
 
     //var player2Keys = keysToIndex ("38", "40", "37", "39");
     var player2 = createPlayer(canvas.width/3*2/cellDimension,
@@ -154,18 +171,15 @@ function tron(){
                               "#00FF40",
                               direction.left,
     //                          player2Keys,
-                              0);
+                              wins2);
 
      function onKeyDown(e) {
                     //let key = event.which || event.keyCode || 0;
                     //"87", "90", "65", "83"
                     let key = e.keyCode;
                     console.log(key);
+                    console.log(direction[keysToIndex[key]]);
                     switch (key) {
-                      case 13 :
-                          isInGame = true;
-                          ctx.clearRect(0,0,600,600);
-                          break;
                       case 87 :
                       case 83 :
                       case 65 :
@@ -187,13 +201,52 @@ function tron(){
                     // }
     }
 
-    //initializeNewLevel();
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     document.addEventListener("keydown", onKeyDown);
 
-    setInterval(tronGameLoop, speedIndex, player1, player2);
+    var interval = setInterval(tronGameLoop, speedIndex, player1, player2);
+
+    function tronGameLoop (player1, player2) {
+          player1.paint();
+          player2.paint();
+
+          if (player1.hasCollided(player2)&&player2.hasCollided(player1)) {
+            paintBoom(player1.position);
+            return;
+          }
+          else if (player1.hasCollided(player2)) {
+            paintBoom(player1.position);
+            player2.wins += 1;
+            return;
+          }
+          if (player2.hasCollided(player1)) {
+            paintBoom(player2.position);
+            player1.wins += 1;
+            return;
+          }
+          player1.move();
+          player2.move();
+          // if (player1.hasCollided(player2)||player2.hasCollided(player1)) {
+          //   document.removeEventListener("keydown", onKeyDown);
+          //   if (player1.wins>=3||player2.wins>=3) {
+          //     //final screen with results player__.wins*pointsPerWin;
+          //     var winner = player1.wins>player2.wins ? 1 : 2;
+          //     printOnCanvas("Tron winner is player" + winner);
+          //     //waitForKey();
+          //     clearInterval(interval);
+          //   }
+          //   else {
+          //     printOnCanvas("Press a key to play again");
+          //     //waitForKey();
+          //     clearInterval(interval);
+          //     playTronGame(player1.wins, player2.wins);
+          //   }
+          //
+          // }
+    }
   }
 
 
-    playTronGame();
+    playTronGame(0, 0);
 
 }
